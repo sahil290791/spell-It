7e04e6964709bfe34255eab0d747a3500f9c991b
Sahil <sahilprjpt206@gmail.com>
2015-08-13 01:17:51 +0530

    fixed demo page

--- dist/spellIt.js
+++ dist/spellIt.js
@@ -20,9 +20,6 @@
 				this.settings = $.extend( {}, defaults, options );
 				this._defaults = defaults;
 				this._name = pluginName;
-				this.mainString = this.element.innerHTML;
-				this.words = new Array;
-				this.actual_code = new Array;
 				this.lowercaseWord = new Array;
 				this.terms = {"a" : "Alfa", "b": "Bravo", "c": "Charlie", "d": "Delta", "e": "Echo", "f": "Foxtrot", "g": "Golf", "h": "Hotel", "i": "India", "j": "Juliett", "k": "Kilo","l": "Lima", "m": "Mike", "n": "November", "o": "Oscar", "p": "Papa", "q": "Quebec", "r": "Romeo", "s": "Sierra", "t": "Tango", "u": "Uniform", "v": "Victor", "w": "Whiskey", "x": "X-ray", "y": "Yankee", "z": "Zulu", "0": "Zero", "1": "One", "2": "Two", "3": "Three", "4": "Four", "5": "Five", "6": "Six", "7": "Seven", "8": "Eight", "9": "Nine", ".": "Dot", "@": "At Sign", "?": "Question Mark", "!": "Exclamation Mak", "&" : "Ampersand", "-": "Minus", "_": "Underscore", "*": "Star", "$": "Dollar", "#": "Hash", "%": "Percentage", "^": "Caret", "\u20AC": "Euro Sign", "~": "Tilde", ";": "Semicolon", ":" : "Colon", "(": "Left Bracket", ")": "Right Bracket", "|": "Vertical Bar/Pipe", "+": "Plus Sign", ",": "Comma"};
 				this.init();
@@ -30,6 +27,9 @@
 		$.extend(Plugin.prototype, {
 				init: function () {
 						var string = [];
+						this.mainString = this.element.innerHTML;
+						this.words = new Array;
+						this.actual_code = new Array;
 						this.actual_code = this.mainString.split('');
 						this.lowercaseWord = this.mainString.toLowerCase().split('');
 			            string = this.lowercaseWord;
@@ -53,6 +53,13 @@
 			            }
 			            return obj;
 		        },
+		        destroy: function(){
+		        	this.mainString = "";
+					this.words = "";
+					this.actual_code = "";
+					this.actual_code = "";
+					this.lowercaseWord = "";
+		        },
 		        showOutput: function(){
 		        	var container = $('<table />');
 		        	var data = "<thead><tr><th>Character</th><th>Spellified</th></tr></thead><tbody>";
@@ -62,15 +69,17 @@
 		        	}
 		        	data += "</tbody>";
 		        	container.addClass(this.settings.className).append(data);
-		        	$(this.settings.target).append(container);
+		        	$(this.settings.target).html('').append(container);
+		        	this.destroy();
 		        }
 
 		});
 		$.fn[ pluginName ] = function ( options ) {
 				return this.each(function() {
-						if ( !$.data( this, "plugin_" + pluginName ) ) {
-								$.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
-						}
+						// if ( !$.data( this, "plugin_" + pluginName ) ) {
+						// 		$.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
+						// }
+						new Plugin( this, options );
 				});
 		};
 
