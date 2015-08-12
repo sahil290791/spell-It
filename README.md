1b53eea80ae18bee84545453c0861ae24e1da860
Sahil <sahil.prajapati@tcs.com>
2015-08-12 16:35:06 +0530

    pushing spellIt

---
+++ README.md
@@ -0,0 +1,51 @@
+### A new way to 
+
+
+
+## Usage
+
+1. Include jQuery:
+
+	```html
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+	```
+
+2. Include plugin's code:
+
+	```html
+	<script src="dist/spellit.min.js"></script>
+	```
+
+3. Call the plugin:
+
+	```javascript
+	$("#targer").spellIt({
+		className: "spellify",//optional class to be added to the table
+		target: "result",//id name
+		getTabulatedResult: true // appends a table to the specified target
+	});
+	```
+## Guides
+
+Available options: 
+	className: "anyClass" ( Use this option to add a class to the table created.)
+	target: "target-ID" (Specify the ID of the element to which the result should be appended to.)
+	getTabulatedResult: true (true by default, returns a HTML table appended to the target
+							, if set to false it will give an array of the spellified characters)
+
+#### Examples
+
+Coming Soon 
+
+
+## Contributing
+
+Check [CONTRIBUTING.md](https://github.com/jquery-boilerplate/boilerplate/blob/master/CONTRIBUTING.md) for more information.
+
+## History
+
+Check [Releases](https://github.com/jquery-boilerplate/jquery-boilerplate/releases) for detailed changelog.
+
+## License
+
+[MIT License](http://zenorocha.mit-license.org/) © Zeno Rocha
---
+++ src/spellIt.js
@@ -0,0 +1,79 @@
+/*
+ *  Made by Sahil Prajapati
+ *  Under MIT License
+ */
+// the semi-colon before function invocation is a safety net against concatenated
+// scripts and/or other plugins which may not be closed properly.
+;(function ( $, window, document, undefined ) {
+
+	"use strict";
+		// Create the defaults once
+		var pluginName = "spellIt",
+						defaults = {
+							className: "spellIt",//class to be added to table
+							target: "result",//id name
+							getTabulatedResult: true
+						};
+		
+		function Plugin ( element, options ) {
+				this.element = element;
+				this.settings = $.extend( {}, defaults, options );
+				this._defaults = defaults;
+				this._name = pluginName;
+				this.mainString = this.element.innerHTML;
+				this.words = new Array;
+				this.actual_code = new Array;
+				this.lowercaseWord = new Array;
+				this.terms = {"a" : "Alfa", "b": "Bravo", "c": "Charlie", "d": "Delta", "e": "Echo", "f": "Foxtrot", "g": "Golf", "h": "Hotel", "i": "India", "j": "Juliett", "k": "Kilo","l": "Lima", "m": "Mike", "n": "November", "o": "Oscar", "p": "Papa", "q": "Quebec", "r": "Romeo", "s": "Sierra", "t": "Tango", "u": "Uniform", "v": "Victor", "w": "Whiskey", "x": "X-ray", "y": "Yankee", "z": "Zulu", "0": "Zero", "1": "One", "2": "Two", "3": "Three", "4": "Four", "5": "Five", "6": "Six", "7": "Seven", "8": "Eight", "9": "Nine", ".": "Dot", "@": "At Sign", "?": "Question Mark", "!": "Exclamation Mak", "&" : "Ampersand", "-": "Minus", "_": "Underscore", "*": "Star", "$": "Dollar", "#": "Hash", "%": "Percentage", "^": "Caret", "\u20AC": "Euro Sign", "~": "Tilde", ";": "Semicolon", ":" : "Colon", "(": "Left Bracket", ")": "Right Bracket", "|": "Vertical Bar/Pipe", "+": "Plus Sign", ",": "Comma"};
+				this.init();
+		}
+		$.extend(Plugin.prototype, {
+				init: function () {
+						var string = [];
+						this.actual_code = this.mainString.split('');
+						this.lowercaseWord = this.mainString.toLowerCase().split('');
+			            string = this.lowercaseWord;
+			            for (var i=0; i < string.length; i++){
+			                this.words.push(this.terms[string[i]]);
+			            }
+			            if (this.settings.getTabulatedResult === true){
+			            	return this.showOutput();
+			            }
+			            else{
+			            	return this.words;
+			            }
+				},
+		        splitCode: function(){
+		        		console.log('words is '+this.actual_code);
+			            return this.actual_code;
+		        },
+		        getCodeHash: function() {
+			            var obj = {};
+			            for (var i=0; i< this.words.length; i++){
+			                obj[this.actual_code[i]] = this.words[i];
+			            }
+			            console.log('obj '+obj);
+			            return obj;
+		        },
+		        showOutput: function(){
+		        	var container = $('<table />');
+		        	var data = "<thead><tr><th>Character</th><th>Spellified</th></tr></thead><tbody>";
+		        	for (var i=0;i < this.actual_code.length; i++){
+		        		data += "<tr><th>"+ this.actual_code[i] +"</th>";
+		        		data += "<td>"+ this.words[i]+"</td></tr>";
+		        	}
+		        	data += "</tbody>";
+		        	container.addClass(this.settings.className).append(data);
+		        	$('#'+this.settings.target).append(container);
+		        }
+
+		});
+		$.fn[ pluginName ] = function ( options ) {
+				return this.each(function() {
+						if ( !$.data( this, "plugin_" + pluginName ) ) {
+								$.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
+						}
+				});
+		};
+
+})( jQuery, window, document );
